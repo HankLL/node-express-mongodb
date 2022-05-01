60c4c408eb6ca0d35fb56a3f3f481ba2706e4100
HankLL <leehinlung2002@gmail.com>
2022-05-01 12:05:49 +0800

    Full Picture

---
+++ app/controllers/operation.controller.js
@@ -0,0 +1,15 @@
+//0. mutitask system
+//single thread
+//While review?  (multi thread)
+//1. review = true, review account clientInformation
+//1.1 retreive account info
+//1.1.1 account balance cash/debt,stock net value.
+//1.1.2.1 retreive all stock info and update history
+//1.1.2.2 process rule 
+//1.1.2.3 update cache from ruliing result
+//1.1.3 perform revision
+//2. change profolio?
+//3. true >> upadating (call/push)order (multi thread)
+//3.1 connect broker API(futu)
+//3.2 Which order
+//4. finishe revision
